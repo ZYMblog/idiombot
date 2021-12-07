c89f92ac7ba5457424baa71241b075e63416ba52
ZYMblog <zzhaoyiman@gmail.com>
2021-12-08 02:57:23 +0800

    alter production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server "120.46.147.189", user:"deployer", roles: %w{app db web}
+# server "120.46.147.189", user:"deployer", roles: %w{app db web}
 
 
 
